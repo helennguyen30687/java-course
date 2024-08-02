33c10ddd37c18c1c406f54ead30d08d0147ebc0a
helennguyen30687 <helennguyen30687@gmail.com>
2024-08-02 18:07:12 +0700

    Lab 01 VariableDataTypeBasicJava

---
+++ src/lab_01/VariableLearning.java
@@ -0,0 +1,9 @@
+package lab_01;
+
+public class VariableLearning {
+    public static void main(String[] args) {
+        byte myByteNumber=1;
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(myByteNumber);
+    }
+}
