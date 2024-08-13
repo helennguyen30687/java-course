1c18eb8a09489c762f0072464a0ed4390713076e
helennguyen30687 <helennguyen30687@gmail.com>
2024-08-14 06:36:26 +0700

    Lesson 05 Set ArrayList Sublist

--- src/Lesson_05/RemovingDuplicateValues.java
+++ src/Lesson_05/RemovingDuplicateValues.java
@@ -1,7 +1,37 @@
 package Lesson_05;
 
+import java.util.*;
+
 public class RemovingDuplicateValues {
     public static void main(String[] args) {
+        List<Integer> myArrList = new ArrayList<>();
+        myArrList.add(1);
+        myArrList.add(7);
+        myArrList.add(1);
+        myArrList.add(5);
+        myArrList.add(2);
+        myArrList.add(3);
 
+        /*
+         * Lay 3 gia tri nho nhat ko trung nhau
+         * */
+        List<Integer> withoutDuplicatedValuesList = new ArrayList<>();
+        for (Integer number : myArrList) {
+            if (!withoutDuplicatedValuesList.contains(number)) {
+                withoutDuplicatedValuesList.add(number);
+            }
+        }
+        System.out.println("myArrList " + myArrList);
+        Collections.sort(withoutDuplicatedValuesList);
+        System.out.println("withoutDuplicatedValuesList " + withoutDuplicatedValuesList);
+
+        //Set
+        Set<Integer> set = new HashSet<>(myArrList);
+        myArrList = new ArrayList<>(set);
+        System.out.println(myArrList);
+
+        //sublist
+        List<Integer> first3MinValues= withoutDuplicatedValuesList.subList(0,3); //index at 3 is exclusive
+        System.out.println("first3MinValues "+first3MinValues);
     }
 }
