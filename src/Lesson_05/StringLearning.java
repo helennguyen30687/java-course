d679227535419156c688db1ef4ccb0b4b1db5c77
helennguyen30687 <helennguyen30687@gmail.com>
2024-08-20 06:13:42 +0700

    Lesson 05 HashMap

--- src/Lesson_05/StringLearning.java
+++ src/Lesson_05/StringLearning.java
@@ -1,5 +1,7 @@
 package Lesson_05;
 
+import java.util.Arrays;
+
 public class StringLearning {
     public static void main(String[] args) {
         //Check lowercase, uppercase, digit...
@@ -15,10 +17,43 @@ public class StringLearning {
             else if (Character.isUpperCase(character)) totalUpperCase++;
         }
 
-        if(totalDigits>0 && totalLowerCase >0 && totalUpperCase >0){
+        if (totalDigits > 0 && totalLowerCase > 0 && totalUpperCase > 0) {
             System.out.println("You are all set!");
-        }
-        else System.out.println("Wrong format password");
+        } else System.out.println("Wrong format password");
+        //Replace, immutable
+        String badWordContainer = "           bad, very bad, sth else, bad ";
+        String filterStr = badWordContainer.replace("bad", "b**");
+        System.out.println(badWordContainer);
+        System.out.println(filterStr);
 
+        //Trim
+        System.out.println("--------------------");
+        System.out.println(badWordContainer.trim());
+
+        //Substring, indexOf, split
+        String url = "☺https://google.com";
+        System.out.println(url.length());
+        System.out.println(url.indexOf("ht"));
+        System.out.println(url.indexOf("w"));
+        System.out.println(url.indexOf("☺"));
+
+        System.out.println(url.substring(2, url.length()));
+        System.out.println(url.substring(0, 3));
+
+        String[] splitStr = url.split("");
+        System.out.println(Arrays.toString(splitStr));
+
+        //Regex | Regular expression
+        String myCookingTimes = "          105 mins            ";
+
+        // TODO: Pattern and Matcher
+        String cookingTimeNumStr = myCookingTimes.replaceAll("[^0-9]", "");
+        System.out.println(cookingTimeNumStr);
+        System.out.println(cookingTimeNumStr + 1);
+
+        System.out.println(Integer.valueOf(cookingTimeNumStr) + 1);
+        System.out.println("My cooking time: " + (cookingTimeNumStr + 1));
+
+        //StringBuilder
     }
 }
