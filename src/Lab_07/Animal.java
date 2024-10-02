d7bd992a4bfc44fd1906c75a72c08ca080231c2c
unknown <helennguyen30687@gmail.com>
2024-10-02 16:41:15 +0700

    Lesson 07 OOP exercise

---
+++ src/Lab_07/Animal.java
@@ -0,0 +1,22 @@
+package Lab_07;
+
+import java.util.Random;
+
+public class Animal {
+    private int speed;
+
+    public Animal() {
+    }
+
+    public Animal(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
--- src/Lab_07/SalaryController.java
+++ src/Lab_07/SalaryController.java
@@ -1,5 +1,13 @@
 package Lab_07;
 
-public class SalaryController {
+import java.util.List;
 
+public class SalaryController {
+    public int getTotalSalary(List<Employee> employeeList) {
+        int totalSalary = 0;
+        for (Employee employee : employeeList) {
+            totalSalary += employee.getSalary();
+        }
+        return totalSalary;
+    }
 }
